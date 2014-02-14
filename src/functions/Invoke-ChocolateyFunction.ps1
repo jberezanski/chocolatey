0feada89b641fbb48671c4bb41da8c39433df31f
Jakub Bereżański <kuba@berezanscy.pl>
2014-02-14 21:03:15 +0100

    Invoke-ChocolateyFunction: improve parameter logging
    - print parameters passed as hashtable correctly
    
    - nicely format parameter values, so that the output resembles the
      actual syntax that would be used when invoking the function directly

--- src/functions/Invoke-ChocolateyFunction.ps1
+++ src/functions/Invoke-ChocolateyFunction.ps1
@@ -1,6 +1,19 @@
 function Invoke-ChocolateyFunction ($ChocoFunction,$paramlist) {
   try {
-    Write-Debug "Invoke-ChocolateyFunction is calling: `$ChocoFunction='$ChocoFunction'|`@paramlist='@paramlist'"
+    $paramstr = ''
+    $formatter = { param($x) if ($x -is [string]) { "'$($x)'" } else { if ($x -eq $null) { '$null' } else { $x } } }
+    if ($paramlist -is [array]) {
+      $paramstr = '(' + (($paramlist | % { & $formatter $_ }) -join ',') + ')'
+    } else {
+      if ($paramlist -is [hashtable]) {
+        # note: no way to distinguish between [switch] and [bool] parameters without reflecting on $ChocoFunction;
+        # string displayed for switches will be syntactically incorrect, but will show correct value, e.g. '-uninstall True'
+        $paramstr = ($paramlist.GetEnumerator() | % { '-' + $_.Name + ' ' + (& $formatter $_.Value) }) -join ' '
+      } else {
+        $paramstr = "'$paramlist'"
+      }
+    }
+    Write-Debug "Invoke-ChocolateyFunction is calling: `$ChocoFunction='$ChocoFunction'|`@paramlist=$paramstr"
     invoke-expression "$ChocoFunction @paramlist;"
   }
   #catch {Write-Host $_.exception.message -BackgroundColor Red -ForegroundColor White ;exit 1}
