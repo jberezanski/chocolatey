jberezanski/chocolatey

commit b12e91bfda42b74d55e0b3c7cc3446290a112bb2

add error level checking to chocolatey install process (nuget)
provide the try/catch/throw bubble up functionality to the chocolatey
install process around nuget

src/functions/Chocolatey-Uninstall.ps1
@@ -10,16 +10,16 @@ param(
 	Write-ChocolateyFailure "Chocolatey-Uninstall" "Missing PackageName input parameter."
 	return
   }
-  
+
   if ($packageName -eq 'all') { 
     write-host "Uninstalling all packages is not yet supported in this version. "  
 	# by default this should prompt user 2x.  Also can provide a -nuke switch for prompt bypass
     return
   }
-  
+
     Write-Host "Chocolatey (v$chocVer) is uninstalling $packageName..." -ForegroundColor $RunNote -BackgroundColor Black
-  
-	
+
+
 	$packages = $packageName
 	foreach ($package in $packages) {
 		$versions = Chocolatey-Version $package $source
@@ -30,8 +30,8 @@ param(
 		  Write-Debug "Looking for $($package).$($versions.found)"
           $packageFolder = Join-Path $nugetLibPath "$($package).$($versions.found)" 
           Write-host "Uninstalling from folder $packageFolder"
-          Get-ChocolateyBins $packageFolder -uninstall
-          Run-ChocolateyPS1 $packageFolder $package "uninstall"
+          Invoke-ChocolateyFunction "Get-ChocolateyBins" @($packageFolder,"-uninstall")
+          Invoke-ChocolateyFunction "Run-ChocolateyPS1" @($packageFolder,$package,"uninstall")
 		  Remove-Item -Recurse -Force $packageFolder
 		}
 	}

src/functions/Chocolatey-Install.ps1
@@ -20,6 +20,6 @@ param(
     "cygwin" { Chocolatey-Cygwin $packageName $installerArguments; }
     "python" { Chocolatey-Python $packageName $version $installerArguments; }
     "ruby" { Chocolatey-RubyGem $packageName $version $installerArguments; }
-    default { Chocolatey-NuGet $packageName $source $version $installerArguments; }
+    default {Invoke-ChocolateyFunction "Chocolatey-Nuget" @($packageName,$source,$version,$installerArguments)}
   }
 }

src/functions/Chocolatey-InstallAll.ps1
@@ -21,14 +21,15 @@ param(
     $entries = $feed | select -ExpandProperty feed | select -ExpandProperty entry  
     foreach ($entry in $entries) {
       if ($entry.properties.id -ne $null) {
-        Chocolatey-NuGet $entry.properties.id -version $entry.properties.version -source $source
+        Invoke-ChocolateyFunction "Chocolatey-Nuget" @("$entry.properties.id","-version $entry.properties.version","-source $source") 
+
       }
     } 
   } else {
     $files = get-childitem $source -include *.nupkg -recurse
     foreach ($file in $files) {
       $packageName = $file.Name -replace "(\.\d{1,})+.nupkg"
-      Chocolatey-NuGet $packageName -source $source
+      Invoke-ChocolateyFunction "Chocolatey-Nuget" @($packageName," -source $source") 
     }
   }
 }

src/functions/Chocolatey-InstallIfMissing.ps1
@@ -15,6 +15,6 @@ param(
   $versions = Chocolatey-Version $packageName $source
 
   if ($versions.'found' -contains 'no version' -or ($version -ne '' -and $versions.'found' -ne $version)) {
-    Chocolatey-NuGet $packageName $source $version
+    Invoke-ChocolateyFunction "Chocolatey-Nuget" @($packageName,$source,$version) 
   }
 }

src/functions/Chocolatey-PackagesConfig.ps1
@@ -10,7 +10,7 @@ param(
     Write-Debug "No file exists at `'$packagesConfigPath`'"
     if (-not($($packagesConfigPath).Contains('\'))) {
       Write-Debug "Going to attempt to install $packagesConfigPath as regular chocolatey package."
-      Chocolatey-NuGet $packagesConfigPath
+      Invoke-ChocolateyFunction "Chocolatey-Nuget" @($packagesConfigPath)
     }
     
     return

src/functions/Chocolatey-Update.ps1
@@ -8,18 +8,18 @@ param(
   
   $packages = $packageName
   if ($packageName -eq 'all') {
-		$packageFolders = Get-ChildItem $nugetLibPath | sort name
-		$packages = $packageFolders -replace "(\.\d{1,})+"|gu 
-	}
+    $packageFolders = Get-ChildItem $nugetLibPath | sort name
+    $packages = $packageFolders -replace "(\.\d{1,})+"|gu 
+  }
 
   foreach ($package in $packages) {
     $versions = Chocolatey-Version $package $source
     if ($versions -ne $null -and $versions.'foundCompare' -lt $versions.'latestCompare') {
         Chocolatey-NuGet $package $source
     } elseif ($versions -ne $null -and $force -and $versions.'foundCompare' -eq $versions.'latestCompare') {
-        Chocolatey-NuGet $package $source
+        Invoke-ChocolateyFunction "Chocolatey-Nuget" @($package,$source)
     } else {
       Write-Debug "$packageName - you have either a newer version or the same version already available"
     }
-	}
+  }
 }

src/functions/Run-ChocolateyPS1.ps1
@@ -60,7 +60,7 @@ param(
     }
 
     if ($installps1 -notlike '' -and $ps1 -like '') {
-      Write-Host "This package has a chocolateyInstall.ps1 without a chocolateyUninstall.ps1. You will need to manually reverse whatever steps the installer did. Please ask the package maker to include a chocolateyUninstall.ps1 in the file to really remove the package." -ForegroundColor $Warning -BackgroundColor Black
+      throw "This package has a chocolateyInstall.ps1 without a chocolateyUninstall.ps1. You will need to manually reverse whatever steps the installer did. Please ask the package maker to include a chocolateyUninstall.ps1 in the file to really remove the package."
     }
   }
 }

src/functions/Run-ChocolateyProcess.ps1
@@ -18,6 +18,6 @@ param(
   $s = [System.Diagnostics.Process]::Start($psi);
   $s.WaitForExit();
   if ($s.ExitCode -ne 0) {
-    Write-Host "[ERROR] Running $file with $arguments was not successful." -ForegroundColor White -BackgroundColor DarkRed
+    throw "[ERROR] Running $file with $arguments was not successful."
   }
 }

commit 1eaecef44c3e73bfa13940f8ff51ba91da40cd75

Chocolatey-Uninstall: fix passing -uninstall switch to Get-ChocolateyBins
This fixes batch redirect files not being removed when uninstalling a
package (a bug introduced by b12e91bf "add error level checking...").

Invoke-ChocolateyFunction uses the splatting operator ("@") to pass
parameters to the invoked function. When the parameter values are passed to
Invoke-ChocolateyFunction encapsulated in an array, the splatting operator
matches them only to positional parameters of the invoked function. In
order to pass values of named parameters and/or switches, they need to be
encapsulated in a hashtable.

src/functions/Chocolatey-Uninstall.ps1
@@ -30,7 +30,7 @@ param(
 		  Write-Debug "Looking for $($package).$($versions.found)"
           $packageFolder = Join-Path $nugetLibPath "$($package).$($versions.found)" 
           Write-host "Uninstalling from folder $packageFolder"
-          Invoke-ChocolateyFunction "Get-ChocolateyBins" @($packageFolder,"-uninstall")
+          Invoke-ChocolateyFunction "Get-ChocolateyBins" @{packageFolder=$packageFolder;uninstall=$true}
           Invoke-ChocolateyFunction "Run-ChocolateyPS1" @($packageFolder,$package,"uninstall")
 		  Remove-Item -Recurse -Force $packageFolder
 		}