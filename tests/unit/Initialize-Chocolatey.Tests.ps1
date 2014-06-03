df27623b6fd7f48016bdc7e8c3d1312f186e6690
Jakub Bereżański <kuba@berezanscy.pl>
2014-06-03 23:56:06 +0200

    Enable mocking Test-AdminRights for setup tests
    As Chocolatey setup code explicitly imports the helpers module, Pester
    mocking mechanisms do not work (the mocks are overridden when the helpers
    are imported again).
    
    Mocking the admin rights check in setup tests is critical to making it
    possible to test Chocolatey setup at various user right levels. The only
    way this developer has found to achieve that was to implement a test hook
    in the helper itself.

--- tests/unit/Initialize-Chocolatey.Tests.ps1
+++ tests/unit/Initialize-Chocolatey.Tests.ps1
@@ -132,6 +132,19 @@ function Execute-WithMockingUAC([string]$status, $scriptBlock)
     }
 }
 
+function Execute-AsMockedNonAdmin($scriptBlock)
+{
+    $global:ChocolateyTestsMockAdminRights = $false
+    try
+    {
+        & $scriptBlock
+    }
+    finally
+    {
+        Remove-Variable ChocolateyTestsMockAdminRights -scope global
+    }
+}
+
 function Remove-EnvironmentVariable($name)
 {
     Write-Debug "Ensuring environment variable $name is not set at any scope"
@@ -302,8 +315,8 @@ function Execute-ChocolateyInstallationInDefaultDir($scriptBlock)
 }
 
 Describe "Initialize-Chocolatey" {
-    # note: the specs below are correct when installing with administrative permissions
-    # the test suite is always run elevated, so no easy way to test limited user install for now
+    # note: the correctness of the specs below is dependent upon all code using Test-AdminRights
+    # the test suite is always run elevated, so the only way to test limited user install is to mock that function
 
     Context "When installing as admin, without UAC, with `$Env:ChocolateyInstall not set and no arguments" {
         Setup-ChocolateyInstallationPackage
